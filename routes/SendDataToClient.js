cd59000551d0deeaff926d3e1ce8dc4421b7a5d3
Aman Rathod <as7677379564@gmail.com>
2021-07-10 08:17:38 +0530

    User.find() fixed issue

--- routes/SendDataToClient.js
+++ routes/SendDataToClient.js
@@ -4,11 +4,9 @@ const router = express.Router();
 const User = require('../models/Login_User');
 const Posts = require('../models/Post');
 
-
 router.get('/userId/:id', async (req, res, next) => {
     const id  = (req.params.id);
     let data = [];
-    console.log('sdfd', id)
 
     try{
         let store = '';
@@ -85,6 +83,83 @@ router.get('/UserByUsername/:username', async (req, res) => {
         const username = req.params.username;
         const data = await User.findOne({username: username});
         res.send(data);
+    } catch (err) {
+        console.error(err);
+    }
+})
+
+router.get('/UserFollow', (req, res) => {
+    const loggedUsername = req.query.loggedInUsername;
+    const profileId = (req.query.profileId);
+    
+    try{
+        let boolValue = false;
+        User.find({}, function(err, user) {
+            user.forEach(function(user){
+                if(user.username === loggedUsername){
+                    if(user.following.includes(profileId)){
+                    boolValue=true;
+                    }
+                }
+            })
+            res.send(boolValue);
+        })
+
+        
+    } catch (err) {
+        console.error(err);
+    }
+})
+
+router.put('/changeFollower', async (req, res) => {
+    const loggedId = req.query.loggedInUsername;
+    const profileId = req.query.profileUsername;
+    const toggleValue = req.query.toggleValue;
+    console.log('logge', loggedId)
+    console.log('profile', profileId)
+
+    const user1 = await User.findOne({_id: loggedId});
+    const user2 = await User.findOne({_id: profileId})
+    //if toggle is true then append the userId else delete
+   
+    try {
+        if(toggleValue === 'true'){
+            
+            if(!user1.following.includes(profileId)){
+                
+                user1.following.push(profileId);
+                user1.save()
+
+            }
+            if(!user2.followers.includes(loggedId)){
+                
+                user2.followers.push(loggedId);
+                user2.save();
+            }
+
+        }else {
+            for(let i = 0; i< user1.following.length; ++i) {
+            
+                if(String(user1.following[i]) === String(profileId)){
+                    user1.following.splice(i, 1);
+                    console.log('user1', user1)
+                    user1.save();
+                    break;
+                }
+            }
+
+            for(let i = 0; i< user2.followers.length; ++i) {
+                if(String(user2.followers[i]) === String(loggedId)) {
+                    user2.followers.splice(i, 1);
+                    console.log('user2', user2);
+                    user2.save();
+                    break;
+                }
+            }
+        }
+        const userss = await User.findById({_id: profileId})
+        res.status(200).json({loggedIn: user1, profile: user2});
+        
     } catch (err) {
         console.error(err);
     }
